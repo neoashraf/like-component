b470a049f9ab44adf5afb344b9bf52d9bb0b354f
neo <ashrafuddin160@gmail.com>
2018-04-15 21:58:32 +0600

    completed like button action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   post = {
-    isFavorite : true,
+    isFavorite : false,
+  }
+  // type anotation for compile time checking.
+  // we can use inline anotation  (eventArgs:{neewValue : boolean}) 
+  // or interface (it is in the component from where the event is raised and we import that)
+  onFavoriteChange(favchangedEventArgs){
+    console.log("Favorite Changed",favchangedEventArgs);
   }
 }
